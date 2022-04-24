d5d999109d21f30c7f8f73ea4dbf1b7511fc81b5
Ethan Ryan <ethanryan9@gmail.com>
2022-04-24 11:13:51 +1000

    Update explainations

--- decisionApp/ui.R
+++ decisionApp/ui.R
@@ -1,10 +1,4 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# User interface
 #
 
 library(shiny)
@@ -19,20 +13,26 @@ shinyUI(navbarPage("DecisonMakerR",
                              # Application title
                              titlePanel("DecisionMakeR"),
                              h5("Compare 2 possible decisions"),
-                             p("1. Choose 5 criteria to compare your options."),
+                             p(strong("1. What criteria are important? "),
+                               "Choose 5 criteria to compare your options."),
                              do.call(fluidRow, purrr::map(1:5, criteriaNameCol)),
-                             p("2. How important is each criteria? Enter a number that indicates each criteria's relative importance."),
+                             p(strong("2. How important is each criteria? "),
+                               "Enter a number that indicates each criteria's relative importance."),
                              do.call(fluidRow, purrr::map(1:5, wtCol)),
-                             p("3. Estimate how well option 1 will go on each criteria, by deciding parameters of a probability distribution."),
+                             p(strong("3. How good will option 1 be? "),
+                               "Estimate how well option 1 will go on each criteria, by deciding parameters of a probability distribution."),
+                             p(em("Suggestion: Choose values so that 0 is neutral, 1 is a strong positive outcome and 1 is a strong negative outcome")),
                              do.call(fluidRow, purrr::map(1:5, paramsCol)),
                              do.call(fluidRow, purrr::map(1:5, distPlotCol)),
-                             p("4. Estimate how well option 2 will go on each criteria, by deciding parameters of a probability distribution."),
+                             p(strong("4. How good will option 2 be? "),
+                               "Estimate how well option 1 will go on each criteria, by deciding parameters of a probability distribution."),
                              
                              do.call(fluidRow, purrr::map(1:5, paramsCol,
                                                           optionNo = 2)),
                              do.call(fluidRow, purrr::map(1:5, distPlotCol,
                                                           optionNo = 2)),
-                             p("Now, go to the comparisons tab to compare the options")
+                             p(strong("5. View results"),
+                               "Now, go to the comparisons tab to compare the options")
                     ),
                     tabPanel("Comparison",
                              fluidRow(
